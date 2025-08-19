744f8650fe85adb7775909a289c4b4b2d91e1b54
DimaLozovoy <112244746+DimaLozovoy@users.noreply.github.com>
2025-08-20 00:06:11 +0300

    add sort

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import Alert from "./modules/alert.js";
-
 import LanguageChange from "./modules/language-change.js";
+import Slider from "./modules/slider.js";
+import Sort from "./modules/sort.js";
 document.addEventListener("DOMContentLoaded", () => {
   new Alert();
   new LanguageChange();
+  new Slider();
+  new Sort();
 });
