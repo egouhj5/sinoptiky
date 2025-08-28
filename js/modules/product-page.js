62fdb890adcb22cc6ddfbfb79f706a4e43f98132
egouhj5 <kn09032010@gmail.com>
2025-08-28 15:33:46 +0300

    added some polishing

---
+++ js/modules/product-page.js
@@ -0,0 +1,8 @@
+// export default class ProductPage {
+//     constructor() {
+//         this.plusBtn = document.querySelector(".buy-now-options__minus-button");
+//         this.minusBtn = document.querySelector(".buy-now-options__quantity-text");
+//         this.itemQuantityText = document.querySelector(".buy-now-options__plus-button");
+//         this.plusBtn.addEventListener
+//     }
+// }
